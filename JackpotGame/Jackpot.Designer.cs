4840d0ed5b37772475a8c66bc81cc3f29da97eb8
LENOVO <LENOVO@DESKTOP-JVPJGSJ>
2023-07-23 23:13:18 +0300

    Add project files.

---
+++ JackpotGame/Jackpot.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace JackpotGame
+{
+    partial class Jackpot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            button1 = new Button();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(32, 58);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(147, 199);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(244, 58);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(147, 199);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(451, 58);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(147, 199);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(244, 292);
+            button1.Name = "button1";
+            button1.Size = new Size(147, 48);
+            button1.TabIndex = 3;
+            button1.Text = "ÇEVİR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Location = new Point(32, 362);
+            label1.Name = "label1";
+            label1.Size = new Size(566, 55);
+            label1.TabIndex = 4;
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // Jackpot
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(662, 458);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Jackpot";
+            Text = "Jackpot";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Button button1;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ JackpotGame/Jackpot.cs
@@ -0,0 +1,82 @@
+﻿using JackpotGame.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JackpotGame
+{
+    public partial class Jackpot : Form
+    {
+        int cevirmeSuresi = 3000;
+        int resimDegisimSuresi = 100;
+        int sayac = 0;
+        List<Bitmap> resimler = new List<Bitmap>()
+        {
+            Resources.karpuz,
+            Resources.kiraz,
+            Resources.kivi,
+            Resources.muz,
+            Resources.armut,
+            Resources.ananas,
+            Resources.cilek,
+            Resources.erik,
+            Resources.seftali,
+
+        };
+        public Jackpot()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            Random rnd = new Random();
+            int rndSayi = rnd.Next(0, resimler.Count);
+            pictureBox1.Image = resimler[rndSayi];
+            rndSayi = rnd.Next(0, resimler.Count);
+            pictureBox2.Image = resimler[rndSayi];
+            rndSayi = rnd.Next(0, resimler.Count);
+            pictureBox3.Image = resimler[rndSayi];
+            rndSayi = rnd.Next(0, resimler.Count);
+
+            if (sayac == (cevirmeSuresi / resimDegisimSuresi))
+            {
+                timer1.Stop();
+                sayac = 0;
+                KontrolEt();
+            }
+        }
+        private void KontrolEt()
+        {
+            if (pictureBox1.Image == pictureBox2.Image && pictureBox1.Image == pictureBox3.Image)
+            {
+                label1.Text = "Tebrikler! KAZANDINIZ";
+                label1.ForeColor = Color.Green;
+            }
+            else if (pictureBox1.Image == pictureBox2.Image || pictureBox1.Image == pictureBox3.Image || pictureBox2.Image == pictureBox3.Image)
+            {
+                label1.Text = "Amorti Kazandınız,Yatırdığınız Miktar İade Edildi";
+                label1.ForeColor = Color.Orange;
+            }
+            else
+            {
+                label1.Text = "Kaybettiniz! Tekrar Deneyiniz...";
+                label1.ForeColor = Color.Red;
+            }
+        }
+    }
+}
